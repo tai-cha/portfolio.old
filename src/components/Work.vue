9f2f948325a6901224be2c2329183125078aebdb
taichanNE30 <ne300209@senshu-u.jp>
2021-05-03 20:16:45 +0900

    APIからworksを取得するように変更

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -4,11 +4,11 @@
     <div class="img-container">
       <img
         v-if="work.image"
-        :src="require(`@/assets/images/${work.image}`)"
+        :src="work.image.url"
         :alt='`"${work.title}"の画像`'
       />
     </div>
-    <div class="description" v-html="formedDescription" />
+    <div class="description" v-html="work.description" />
     <a
       v-if="work.url"
       :href="work.url"
@@ -27,11 +27,8 @@
       }
     },
     computed: {
-      formedDescription() {
-        if (!(this.work && this.work.description)) return ''
-        return this.work.description.replace("\n", "\n<br />")
-      }
-    }
+    },
+    
   };
 </script>
 
